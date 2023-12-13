7678673ccbf2539c196d5c173076bdcd7a6c89f8
Tolga Kargili <tolga.kargili@gmail.com>
2023-12-13 11:20:12 +0100

    Main.java korrigiert

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,10 +4,11 @@ import static java.lang.String.format;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
 
+        System.out.println("Jetzt geht's los");
+
         int count =0;
         while (true) {
             Thread.sleep(2*1000);
-            System.out.println("Jetzt gehts los");
             System.out.println(format("App running ... %d", count++));
         }
     }
